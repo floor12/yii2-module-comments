aaad7917fdddb0c6b6a82d77824afe0872b4404f
floor12 <floor12@floor12.net>
2019-06-18 12:57:41 +0200

    Возвращение перевода.

---
+++ src/messages/ru/comments.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: floor12
+ * Date: 05.08.2018
+ * Time: 19:08
+ */
+
+return [
+    'Comments' => 'Комментарии',
+    'New comment' => 'Оставить комментарий',
+    'Send me new comments in this thread to email.' => 'Отправлять мне на почту новые комментарии из этой ветки.',
+    'Send' => 'Отправить',
+    'answer' => 'ответить',
+    'Thank you! Your comment will be published after moderation.' => 'Спасибо! Ваше комментарий будет опубликован после модерации.',
+    'Thank you! Your comment is published!' => 'Спасибо! Ваш комментарий опубликован.',
+    'Parent comment not found.' => 'Родительский комментарий не найден.',
+    'Only authorized user can create a comment.' => 'Только авторизованные пользователи могут оставлять комментарии.',
+    'The comment is delited.' => 'Комментарий удален.',
+    'The comment is updated.' => 'Комментарий обновлен.',
+    'search in comments...' => 'поиск по комментариям...',
+    'any status' => 'все статус',
+    'all types of objects' => 'все типы объектов',
+    'Edit this comment' => 'Редактировать этот комментарий',
+    'Delete this comment' => 'Удалить этот комментарий',
+    'Approve this comment' => 'Одобрить этот комментарий',
+    'Save' => 'Сохранить',
+    'Cancel' => 'Отмена',
+    'Comment update' => 'Редактирование комментария',
+    'Status' => 'Статус',
+    'Author name' => 'Имя автора',
+    'Author email' => 'Email автора',
+    'Comment text' => 'Текст комментария',
+    'Published' => 'Опубликован',
+    'Pending' => 'Ожидает модерации',
+    'Deleted' => 'Удален',
+    'This comments is not pending.' => 'Этот комментарий не ожидает модерации.',
+    'New comment is waiting for moderation.' => 'Новый комментарий ожидает модерации.',
+    'Сomment module email robot' => 'Почтовый робот модуля комментариев',
+    'To moderate it click' => 'Для перехода к модерации нажмите на',
+    'this link' => 'эту ссылку',
+    'Your comment was approved.' => 'Ваш комментарий был одобрен.',
+    'The comment is available on' => 'Комментарий доступен на',
+    'this page' => 'этой странице',
+    'New comment in thread' => 'Новый комментарий в ветке',
+    'To unsubscribe from comments of current thread press' => 'Чтобы отписаться от кооментариев этой ветки нажмите на',
+    'Your email is unsubscribed.' => 'Ваш email отписан.',
+    'enter the comment here...' => 'впишите свой комментарий сюда...',
+    'There are no comments yet. Be the first who will post a comment!' => 'Тут еще ничего нет. Будьте первым, кто оставит комментарий!',
+    'Author phone' => 'Номер телефона',
+    'This field is required.' => 'Это поле обязательное',
+    'Attachments' => 'Файлы',
+    'Add new comment' => 'Добавить комментарий',
+    'delete' => 'удалить',
+    'edit' => 'редактировать'
+];
